338444532fcef88d4a733092429b0f8c07675d84
Bronuh <bronyx@yandex.ru>
2023-05-11 18:47:38 +0300

    More boilerplate

--- Scripts/Current/WorldNode.cs
+++ Scripts/Current/WorldNode.cs
@@ -3,10 +3,16 @@ using Scellecs.Morpeh;
 public partial class WorldNode : Node2D
 {
 	public World World { get; private set; }
+	public SystemsGroup Systems { get; private set; }
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		World = World.Create();
+		Systems = World.CreateSystemsGroup();
+		
+		Systems.Initialize();
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,5 +27,13 @@ public partial class WorldNode : Node2D
 		World.FixedUpdate((float)delta);
 		World.LateUpdate((float)delta);
 		World.CleanupUpdate((float)delta);
+	}
+
+	public override void _ExitTree()
+	{
+		Systems?.Dispose();
+		Systems = null;
+		World?.Dispose();
+		World = null;
 	}
 }
