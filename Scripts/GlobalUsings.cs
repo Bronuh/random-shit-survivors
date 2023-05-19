10acea3572250888dad130d1c8c3fa0b4bc39244
Bronuh <bronyx@yandex.ru>
2023-05-19 16:06:07 +0300

    It was a mistake

--- Scripts/GlobalUsings.cs
+++ Scripts/GlobalUsings.cs
@@ -4,6 +4,5 @@ global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
 global using Godot;
-global using System.Numerics;
 global using static Scripts.Common.GodotExtensions;
 global using static Scripts.Libs.EnumerableExtensions;
