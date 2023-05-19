555652159267af2b50208ac3c6fabb01162484fe
Bronuh <bronyx@yandex.ru>
2023-05-19 15:28:13 +0300

    #15 #19 #20

--- Scripts/Libs/EnumerableExtensions.cs
+++ Scripts/Libs/EnumerableExtensions.cs
@@ -48,6 +48,96 @@ namespace Scripts.Libs
 
 			return selected;
 		}
+
+		/// <summary>
+		/// Splits an IEnumerable into multiple IEnumerables of a specified size.
+		/// </summary>
+		/// <typeparam name="T">The type of elements in the IEnumerable.</typeparam>
+		/// <param name="source">The IEnumerable to split.</param>
+		/// <param name="splitSize">The size of each split IEnumerable.</param>
+		/// <returns>An IEnumerable of IEnumerables, each containing splitSize elements from the source.</returns>
+		public static IEnumerable<IEnumerable<T>> Split<T>(this IEnumerable<T> source, int splitSize)
+		{
+			using (IEnumerator<T> enumerator = source.GetEnumerator())
+			{
+				while (enumerator.MoveNext())
+				{
+					yield return InnerSplit(enumerator, splitSize);
+				}
+			}
+
+		}
+
+		private static IEnumerable<T> InnerSplit<T>(IEnumerator<T> enumerator, int splitSize)
+		{
+			int count = 0;
+			do
+			{
+				count++;
+				yield return enumerator.Current;
+			}
+			while (count % splitSize != 0
+				 && enumerator.MoveNext());
+		}
+
+
+		/// <summary>
+		/// Converts a System.IO.Stream to a byte array.
+		/// </summary>
+		/// <param name="stream">The stream to convert.</param>
+		/// <returns>A byte array containing the data from the stream.</returns>
+		public static byte[] ConvertToByteArray(this System.IO.Stream stream)
+		{
+			var streamLength = Convert.ToInt32(stream.Length);
+			byte[] data = new byte[streamLength + 1];
+
+			//convert to to a byte array
+			stream.Read(data, 0, streamLength);
+			stream.Close();
+
+			return data;
+		}
+
+
+		/// <summary>
+		/// Returns the first element of a sequence, or null if the sequence contains no elements.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of source.</typeparam>
+		/// <param name="values">The sequence to return the first element of.</param>
+		/// <returns>null if source is empty; otherwise, the first element in source.</returns>
+		public static T FirstOrNull<T>(this IEnumerable<T> values) where T : class
+		{
+			return values.DefaultIfEmpty(null).FirstOrDefault();
+		}
+
+
+		/// <summary>
+		/// Flattens an <see cref="IEnumerable"/> of <see cref="String"/> objects to a single string, seperated by an optional seperator and with optional head and tail.
+		/// </summary>
+		/// <param name="strings">The string objects to be flattened.</param>
+		/// <param name="seperator">The seperator to be used between each string object.</param>
+		/// <param name="head">The string to be used at the beginning of the flattened string. Defaulted to an empty string.</param>        
+		/// <param name="tail">The string to be used at the end of the flattened string. Defaulted to an empty string.</param>
+		/// <returns>Single string containing all elements seperated by the given seperator, with optionally a head and/or tail.</returns>
+		public static string Flatten(this IEnumerable<string> strings, string seperator, string head, string tail)
+		{
+			// If the collection is null, or if it contains zero elements,
+			// then return an empty string.
+			if (strings == null || strings.Count() == 0)
+				return String.Empty;
+
+			// Build the flattened string
+			var flattenedString = new StringBuilder();
+
+			flattenedString.Append(head);
+			foreach (var s in strings)
+				flattenedString.AppendFormat("{0}{1}", s, seperator); // Add each element with the given seperator.
+			flattenedString.Remove(flattenedString.Length - seperator.Length, seperator.Length); // Remove the last seperator
+			flattenedString.Append(tail);
+
+			// Return the flattened string
+			return flattenedString.ToString();
+		}
 	}
 
 }
--- Scripts/Libs/StringExtensions.cs
+++ Scripts/Libs/StringExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,107 @@ namespace Scripts.Libs
 {
 	public static class StringExtensions
 	{
+		/// <summary>
+		/// Converts string to enum object
+		/// </summary>
+		/// <typeparam name="T">Type of enum</typeparam>
+		/// <param name="value">String value to convert</param>
+		/// <returns>Returns enum object</returns>
+		public static T ToEnum<T>(this string value)
+			where T : struct
+		{
+			return (T)System.Enum.Parse(typeof(T), value, true);
+		}
+
+
+		/// <summary>
+		/// Determines whether the specified string contains all the specified values.
+		/// </summary>
+		/// <param name="value">The string to search.</param>
+		/// <param name="values">The values to search for.</param>
+		/// <returns>true if all values are found in the string; otherwise, false.</returns>
+		public static bool ContainsAll(this string value, params string[] values)
+		{
+			foreach (string one in values)
+			{
+				if (!value.Contains(one))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+
+		/// <summary>
+		/// Determines whether the specified string contains any of the specified values.
+		/// </summary>
+		/// <param name="value">The string to search.</param>
+		/// <param name="values">The values to search for.</param>
+		/// <returns>true if any value are found in the string; otherwise, false.</returns>
+		public static bool ContainsAny(this string value, params string[] values)
+		{
+			foreach (string one in values)
+			{
+				if (value.Contains(one))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+
+
+		/// <summary>
+		///     Formats a large number to 5 digits (2 decimal places) with a suffix. (e.g. 123,456,789,123 (123 Billion) ->
+		///     123.45B)
+		/// </summary>
+		/// <param name="numberToFormat"> The number to format. </param>
+		/// <param name="decimalPlaces"> The number of decimal places to include - <i> defaults to <c> 2 </c> </i> </param>
+		/// <returns> A <see cref="string" />. </returns>
+		public static string FormatNumber(this long numberToFormat, int decimalPlaces = 2)
+		{
+			// Get the default string representation of the numberToFormat.
+			string numberString = numberToFormat.ToString();
+
+			foreach (NumberSuffix suffix in Enum.GetValues<NumberSuffix>())
+			{
+				// Assign the amount of digits to base 10.
+				double currentValue = 1 * Math.Pow(10, (int)suffix * 3);
+
+				// Get the suffix value.
+				string? suffixValue = Enum.GetName(typeof(NumberSuffix), (int)suffix);
+
+				// If the suffix is the placeholder, set it to an empty string.
+				if ((int)suffix == 0) { suffixValue = string.Empty; }
+
+				// Set the return value to a rounded value with the suffix.
+				if (numberToFormat >= currentValue)
+				{
+					numberString = $"{Math.Round(numberToFormat / currentValue, decimalPlaces, MidpointRounding.ToEven)}{suffixValue}";
+				}
+			}
+
+			return numberString;
+		}
+
+		/// <summary> Suffixes for numbers based on how many digits they have left of the decimal point. </summary>
+		/// <remarks> The order of the suffixes matters! </remarks>
+		private enum NumberSuffix
+		{
+			/// <summary> A placeholder if the value is under 1 thousand </summary>
+			P,
+			/// <summary> Thousand </summary>
+			K,
+			/// <summary> Million </summary>
+			M,
+			/// <summary> Billion </summary>
+			B,
+			/// <summary> Trillion </summary>
+			T,
+			/// <summary> Quadrillion </summary>
+			Q
+		}
 	}
 }
