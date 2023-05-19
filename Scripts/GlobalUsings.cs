ff6d6af2a4bf8ea94d380851028bc4134f4bd67d
Bronuh <bronyx@yandex.ru>
2023-05-19 16:03:45 +0300

    Add numerics to global usings

--- Scripts/GlobalUsings.cs
+++ Scripts/GlobalUsings.cs
@@ -4,5 +4,6 @@ global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
 global using Godot;
+global using System.Numerics;
 global using static Scripts.Common.GodotExtensions;
 global using static Scripts.Libs.EnumerableExtensions;
