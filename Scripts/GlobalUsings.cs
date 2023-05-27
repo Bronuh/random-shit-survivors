82a4f8e47faeca3a339126da5393c4b60953efa7
Bronuh <bronyx@yandex.ru>
2023-05-27 16:11:12 +0300

    Add godot tools for VSCode

--- Scripts/GlobalUsings.cs
+++ Scripts/GlobalUsings.cs
@@ -3,6 +3,7 @@ global using System.Collections.Generic;
 global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
+global using System.IO;
 global using Godot;
 global using static Scripts.Libs.GodotExtensions;
 global using static Scripts.Libs.EnumerableExtensions;
