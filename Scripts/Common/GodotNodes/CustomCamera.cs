cf30aadace65b75dfa0e7a271d29610551ea0838
Bronuh <bronyx@yandex.ru>
2023-06-11 21:22:02 +0300

    Prepare Custom nodes for implementation

---
+++ Scripts/Common/GodotNodes/CustomCamera.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Common.GodotNodes
+{
+	public partial class CustomCamera : Camera2D
+	{
+		public override void _Ready()
+		{
+
+		}
+
+		public override void _Process(double delta)
+		{
+
+		}
+
+		public override void _PhysicsProcess(double delta)
+		{
+
+		}
+	}
+}
---
+++ Scripts/Common/GodotNodes/CustomCharacter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Common.GodotNodes
+{
+	public partial class CustomCharacter : CharacterBody2D
+	{
+		public override void _Ready()
+		{
+
+		}
+
+		public override void _Process(double delta)
+		{
+
+		}
+
+		public override void _PhysicsProcess(double delta)
+		{
+
+		}
+	}
+}
