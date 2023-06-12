51f00a7b4e724ea7c5a231ebedb4468a5a359879
Bronuh <bronyx@yandex.ru>
2023-06-12 11:27:48 +0300

    4.1 Migration and global classes test

---
+++ Scripts/Common/GodotNodes/TestNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Common.GodotNodes
+{
+	[GlobalClass]
+	public partial class TestNode : Node2D
+	{
+		[Export]
+		public TestCustomResource TestResource { get; set; } = new();
+
+
+	}
+}
