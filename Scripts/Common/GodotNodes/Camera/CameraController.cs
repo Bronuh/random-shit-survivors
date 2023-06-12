0bc5978f864260e5d6dbc6dfe33a8064b5e38625
Bronuh <bronyx@yandex.ru>
2023-06-12 17:24:16 +0300

    Some changes

--- Scripts/Common/GodotNodes/Camera/CameraController.cs
+++ Scripts/Common/GodotNodes/Camera/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Godot.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,12 @@ using System.Threading.Tasks;
 
 namespace Scripts.Common.GodotNodes
 {
-	public partial class CustomCamera : Camera2D
+	[GlobalClass]
+	public abstract partial class CameraController : Camera2D
 	{
+		private Camera2D _camera = null;
+
+
 		public override void _Ready()
 		{
 
@@ -21,6 +26,15 @@ namespace Scripts.Common.GodotNodes
 		public override void _PhysicsProcess(double delta)
 		{
 
+		}
+
+		protected virtual void FindCamera()
+		{
+			if (GetParentOrNull<Camera2D>() is null)
+			{
+				Warn($"Parent is null");
+				return;
+			}
 		}
 	}
 }
--- Scripts/Common/GodotNodes/CustomCharacter.cs
+++ Scripts/Common/GodotNodes/CustomCharacter.cs
@@ -8,19 +8,6 @@ namespace Scripts.Common.GodotNodes
 {
 	public partial class CustomCharacter : CharacterBody2D
 	{
-		public override void _Ready()
-		{
 
-		}
-
-		public override void _Process(double delta)
-		{
-
-		}
-
-		public override void _PhysicsProcess(double delta)
-		{
-
-		}
 	}
 }
