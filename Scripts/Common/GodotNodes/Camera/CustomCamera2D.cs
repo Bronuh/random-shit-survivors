a95f8048f656d03261e5b4f8962bda62cc9ae3ca
Bronuh <bronyx@yandex.ru>
2023-06-13 21:53:47 +0300

    Small things too

--- Scripts/Common/GodotNodes/Camera/CustomCamera2D.cs
+++ Scripts/Common/GodotNodes/Camera/CustomCamera2D.cs
@@ -8,6 +8,10 @@ namespace Scripts.Common.GodotNodes.Camera
 {
 	public partial class CustomCamera2D : Camera2D
 	{
+		[Export] public bool UseSmoothing = true;
+		[Export]
+		public float SmoothingMultiplier
+
 		public List<CameraController> cameraControllers = new List<CameraController>();
 		public Vector2 TargetLocation { get; set; }
 
