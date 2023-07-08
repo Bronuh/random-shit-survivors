a451a70e4ad38d5e8a67bdc48fbb6494caab861a
Bronuh <bronyx@yandex.ru>
2023-07-08 16:26:11 +0300

    Remove camera "wheels"

--- Scripts/Common/GodotNodes/Camera/CustomCamera2D.cs
+++ Scripts/Common/GodotNodes/Camera/CustomCamera2D.cs
@@ -9,47 +9,10 @@ namespace Scripts.Common.GodotNodes
 	[GlobalClass]
 	public partial class CustomCamera2D : Camera2D
 	{
-		public enum CameraProcessMode
-		{
-			/// <summary>
-			///		Process on _Process()
-			/// </summary>
-			Frames,
-			/// <summary>
-			///		Process on _PhysicsProcess()
-			/// </summary>
-			Physics
-		}
-
-		/// <summary>
-		///		What method will be used to process the camera.
-		/// </summary>
-		[Export] public CameraProcessMode ProcessOn { get; set; } = CameraProcessMode.Physics;
-
 		/// <summary>
 		///		If true, applies the camera after initialization.
 		/// </summary>
 		[Export] public bool UseOnReady { get; set; } = false;
-
-
-		[ExportGroup("Smoothing")]
-		[Export]
-		public bool UseSmoothing { get; set; } = false;
-
-		/// <summary>
-		///		Determines how strong the smoothing of the movement will be. 1 - no smoothing, 0 - no movement.
-		///		The lower the factor, the slower the camera moves.
-		/// </summary>
-		/// <remarks>
-		///		The camera always tries to move towards the goal, so 1 means that it will go all the way in one tick,
-		///		while 0.5 means that every tick the camera will go only half of the remaining way.
-		/// </remarks>
-		[Export(PropertyHint.Range, "0.0, 1, 0.01")]
-		public float SmoothingFactor
-		{
-			get => _smoothingFactor;
-			set => _smoothingFactor = Math.Clamp(value, 0, 1);
-		}
 
 
 		[ExportGroup("Zoom")]
@@ -98,7 +61,7 @@ namespace Scripts.Common.GodotNodes
 
 		public override void _Process(double delta)
 		{
-			if (ProcessOn is CameraProcessMode.Frames)
+			if (ProcessCallback is Camera2DProcessCallback.Idle)
 				Move(delta);
 
 			// Shake must be always processed here
@@ -107,7 +70,7 @@ namespace Scripts.Common.GodotNodes
 
 		public override void _PhysicsProcess(double delta)
 		{
-			if (ProcessOn is CameraProcessMode.Physics)
+			if (ProcessCallback is Camera2DProcessCallback.Physics)
 				Move(delta);
 		}
 
@@ -223,18 +186,7 @@ namespace Scripts.Common.GodotNodes
 
 		private void Move(double dt)
 		{
-			if (UseSmoothing)
-			{
-				float realSmoothing = _minSmoothing + (_maxSmoothing - _minSmoothing) * SmoothingFactor;
-				// Calculate the interpolation factor based on the SmoothingFactor
-				float interpolationFactor = realSmoothing;
-				// Apply the interpolation factor to smooth the movement
-				Position += (TargetPosition - Position) * interpolationFactor;
-			}
-			else
-			{
-				Position = TargetPosition;
-			}
+			Position = TargetPosition;
 		}
 
 
