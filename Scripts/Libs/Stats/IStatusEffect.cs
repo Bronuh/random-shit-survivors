8dd646b4b4e3c4244249c4042135654b1d7fc2bb
Bronuh <bronyx@yandex.ru>
2023-07-10 20:25:07 +0300

    Rename IStatModifierGiver to IStatusEffect

---
+++ Scripts/Libs/Stats/IStatusEffect.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Scripts.Libs.Stats
+{
+    public interface IStatusEffect
+    {
+		/// <summary>
+		/// Check if associated modifiers is valid for now.
+		/// </summary>
+		/// <returns></returns>
+		public bool IsValid { get; }
+
+		/// <summary>
+		///  Must return list of cloned modifiers
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<StatModifier> GetModifiers();
+    }
+}
