948306a0a154c7ae133a5103defe44905f0e8989
Bronuh <bronyx@yandex.ru>
2023-07-10 20:29:49 +0300

    Implement StatusEffect

---
+++ Scripts/Current/GameTypes/StatusEffect.cs
@@ -0,0 +1,34 @@
+﻿using Scripts.Libs;
+using Scripts.Libs.Stats;
+
+namespace Scripts.Current.GameTypes
+{
+	public class StatusEffect : IStatusEffect, ICloneable<StatusEffect>
+	{
+		public List<StatModifier> ModifierPrototypes { get; private set; } = new List<StatModifier>();
+
+		public double Lifetime { get; private set; } = 0;
+		public bool IsPermanent { get; set; } = false;
+		public bool IsValid => _isValid && (IsPermanent || Lifetime >= 0);
+
+		private bool _isValid = true;
+
+		public StatusEffect Clone()
+		{
+			var effect = new StatusEffect();
+
+			effect._isValid = _isValid;
+			foreach (var prototype in ModifierPrototypes)
+			{
+				effect.ModifierPrototypes.Add(prototype.Clone());
+			}
+
+			return effect;
+		}
+
+		public IEnumerable<StatModifier> GetModifiers()
+		{
+			return ModifierPrototypes;
+		}
+	}
+}
