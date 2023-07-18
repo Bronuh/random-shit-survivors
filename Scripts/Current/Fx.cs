ee6f36330abd531d17a24ad3aa558d2d1172a12e
Bronuh <bronyx@yandex.ru>
2023-07-18 20:14:19 +0300

    Prepare for Effects implementation

---
+++ Scripts/Current/Fx.cs
@@ -0,0 +1,7 @@
+﻿namespace Scripts.Current
+{
+	public static class Fx
+	{
+
+	}
+}
---
+++ Scripts/Current/GameTypes/Effect.cs
@@ -0,0 +1,35 @@
+﻿using Godot;
+using Scripts.Libs;
+
+namespace Scripts.Current.GameTypes
+{
+	public partial class Effect : Node2D
+	{
+		public virtual double Lifetime { get; set; } = 0;
+
+		public event Action OnFinish;
+
+		protected Cooldown cooldown = new Cooldown();
+
+		public override void _Ready()
+		{
+			OnFinish += () => { QueueFree(); };
+			cooldown.OnReady += OnFinish;
+		}
+
+		public override void _Process(double delta)
+		{
+			Update(delta);
+		}
+
+		public override void _PhysicsProcess(double delta)
+		{
+			Update(delta);
+		}
+
+		protected virtual void Update(double delta)
+		{
+			cooldown.Update(delta);
+		}
+	}
+}
